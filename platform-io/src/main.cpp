50ad68bc5754e3df1639ef0dd6a675c9a09eb7d6
Adrien Cognac <adriencognac@Adriens-MBP-2-29.NAT.warszawa.vectranet.pl>
2024-03-05 20:18:15 +0100

    two motors

--- platform-io/src/main.cpp
+++ platform-io/src/main.cpp
@@ -15,34 +15,36 @@
 
 #define dirPin 8
 #define stepPin 9
-// #define dir2Pin 10
-// #define step2Pin 11
+#define dir2Pin 10
+#define step2Pin 11
 #define motorInterfaceType 1
 // Define a stepper and the pins it will use
 AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin); 
-// AccelStepper stepper2 = AccelStepper(motorInterfaceType, step2Pin, dir2Pin); 
+ AccelStepper stepper2 = AccelStepper(motorInterfaceType, step2Pin, dir2Pin); 
 int steps = 5000;
 int direction = 1;
-int width = 0;
+long width = 0;
 bool calibrated = false;
 
 void setup()
 {  
   Serial.begin(9600);
   // Change these to suit your stepper if you want
-  stepper.setMaxSpeed(8000);
-  stepper.setSpeed(5000);
+  stepper.setMaxSpeed(10000);
+  stepper.setSpeed(8000);
   stepper.setAcceleration(100000);
   //stepper.moveTo(5000);
 
-  // stepper2.setMaxSpeed(8000);
-  // stepper2.setAcceleration(1000);
-  // stepper2.moveTo(5000);
+  stepper2.setMaxSpeed(10000);
+  stepper2.setSpeed(8000);
+  stepper2.setAcceleration(100000);
+
 stepper.moveTo(steps);
+stepper2.moveTo(steps);
 
 
    pinMode(2, INPUT_PULLUP); //kontaktron 1
- pinMode(3, INPUT_PULLUP); //kontatkron 2
+   pinMode(3, INPUT_PULLUP); //kontatkron 2
 
  
 }
@@ -64,6 +66,7 @@ void loop()
     //kontaktorn 1 nie wykrywa nic, jedziemy
     if (kontakt1 == 1 && direction == 1 && calibrated == false){
       stepper.move(steps);
+      stepper2.move(steps);
       
     } 
 
@@ -75,12 +78,16 @@ void loop()
     // kontaktron 1 zlapal - zeruj pozycje i zawroc
     if (kontakt1 == 0 && direction == 1 && calibrated == false){
       stepper.setCurrentPosition(0);
+      stepper2.setCurrentPosition(0);
       direction = 0;
+      Serial.print("zeroing: ");
+      Serial.println(stepper.currentPosition());
     }
 
     //jedziemy w druga strone
     if (kontakt2 == 1 && direction == 0 && calibrated == false){
       stepper.move(-steps);
+      stepper2.move(-steps);
       
 
       // Serial.print("stepCount: ");
@@ -93,12 +100,18 @@ void loop()
       //Serial.println(stepCount);
       
       stepper.stop();
+      stepper2.stop();
+      Serial.print("step position = ");
+      Serial.println(stepper.currentPosition());
       width = stepper.currentPosition();
       delay(1000);
       //pojedzmy teraz na srodek
       //stepper.setAcceleration(1000);
+      Serial.print("calibrated: width = ");
       Serial.println(width);
-      stepper.moveTo(0);
+      stepper.moveTo(width/2);
+      stepper2.moveTo(width/2);
+      
       
    
   
@@ -107,19 +120,21 @@ void loop()
       calibrated = true;
     }
 //popisowa
-    // if (calibrated == true){
-    //   int wdt = width;
-    //    if (stepper.distanceToGo() == 0){
-    //   stepper.moveTo(-wdt);
-    //   wdt = -wdt;
-    //   }
-    // }
+    if (calibrated == true){
+      // stepper.setSpeed(8000);
+      // stepper2.setSpeed(8000);
+      stepper.runSpeedToPosition();
+      stepper2.runSpeedToPosition();
+      // Serial.println(stepper.currentPosition());
+    }
 
+if (calibrated == false){
+  // stepper.setSpeed(8000);
+  // stepper2.setSpeed(8000);
+    stepper.runSpeed();
+    stepper2.runSpeed();
 
-    stepper.run();
-
-
-    
+}
 
     
 }
