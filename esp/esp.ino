de5535346583a3742a69454566c6b36b4ae8d9b6
aCognac <adrian.kaniak@gmail.com>
2025-02-02 12:14:12 +0100

    breaking lines

--- esp/esp.ino
+++ esp/esp.ino
@@ -4,11 +4,9 @@
 
 const char* ssid = "smacznego";
 const char* password = "poker1234";
-
-SoftwareSerial mySerial(13, 12); // RX, TX
+SoftwareSerial mySerial(13, 12);
 ESP8266WebServer server(80);
 String serialLog = "";
-
 const int LED_PIN = LED_BUILTIN;
 unsigned long previousMillis = 0;
 const long interval = 1000;
@@ -17,24 +15,19 @@ bool ledState = false;
 void setup() {
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, HIGH);
-
-  // Blink 5 times during setup
   for (int i = 0; i < 5; i++) {
     digitalWrite(LED_PIN, LOW);
     delay(200);
     digitalWrite(LED_PIN, HIGH);
     delay(200);
   }
-
-  Serial.begin(9600);  // Debug serial
-  mySerial.begin(9600);   // Software serial
-  
+  Serial.begin(9600);
+  mySerial.begin(9600);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
@@ -43,14 +36,13 @@ void setup() {
   server.on("/", handleRoot);
   server.on("/send", HTTP_POST, handleSend);
   server.on("/read", handleRead);
+  server.on("/clear", HTTP_POST, handleClear);
   
   server.begin();
 }
 
 void loop() {
   server.handleClient();
-
-  // Blink LED every second
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
@@ -60,75 +52,93 @@ void loop() {
 }
 
 void handleRoot() {
-  String html = R"(
-<!DOCTYPE html>
-<html>
-<head>
-  <style>
-    body {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      flex-direction: column;
-      font-family: Arial, sans-serif;
-      max-width: 600px;
-      margin: 0 auto;
-      text-align: center;
-    }
-    form {
-      margin: 20px 0;
-    }
-    #serialLog {
-      border: 1px solid #ccc;
-      height: 300px;
-      overflow-y: scroll;
-      width: 100%;
-      text-align: left;
-      padding: 10px;
-    }
-  </style>
-</head>
-<body>
-  <h1>Node MCU Serial Communication</h1>
-  <form id="messageForm">
-    <input type="text" id="messageInput" placeholder="Enter message">
-    <input type="submit" value="Send">
-  </form>
-  <div id="serialLog"></div>
-  <script>
-    document.getElementById('messageForm').addEventListener('submit', function(e) {
-      e.preventDefault();
-      var message = document.getElementById('messageInput').value;
-      fetch('/send', {
-        method: 'POST',
-        body: new URLSearchParams({message: message}),
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
-      });
-      document.getElementById('messageInput').value = '';
-    });
-
-    function updateSerialData() {
-      fetch('/read')
-        .then(response => response.text())
-        .then(data => {
-          document.getElementById('serialLog').innerHTML = data;
-        });
-    }
-    setInterval(updateSerialData, 1000);
-  </script>
-</body>
-</html>
-)";
-  server.send(200, "text/html", html);
+  server.send(200, "text/html", 
+    "<!DOCTYPE html>"
+    "<html>"
+    "<head>"
+    "<style>"
+    "body {"
+    "  display: flex;"
+    "  justify-content: center;"
+    "  align-items: center;"
+    "  flex-direction: column;"
+    "  font-family: Arial, sans-serif;"
+    "  max-width: 600px;"
+    "  margin: 0 auto;"
+    "  text-align: center;"
+    "}"
+    "form { margin: 20px 0; }"
+    "#serialLog {"
+    "  border: 1px solid #ccc;"
+    "  height: 300px;"
+    "  overflow-y: scroll;"
+    "  width: 100%;"
+    "  text-align: left;"
+    "  padding: 10px;"
+    "  white-space: pre-wrap;"           // Added this line
+    "  word-wrap: break-word;"           // Added this line
+    "  font-family: monospace;"          // Added this line
+    "  line-height: 1.4;"               // Added this line
+    "}"
+    ".button {"
+    "  margin: 10px;"
+    "  padding: 5px 15px;"
+    "  cursor: pointer;"
+    "}"
+    "</style>"
+    "</head>"
+    "<body>"
+    "<h1>Node MCU Serial Communication</h1>"
+    "<form id=\"messageForm\">"
+    "<input type=\"text\" id=\"messageInput\" placeholder=\"Enter message\">"
+    "<input type=\"submit\" value=\"Send\">"
+    "</form>"
+    "<button onclick=\"clearSerialLog()\" class=\"button\">Clear Output</button>"
+    "<div id=\"serialLog\"></div>"
+    "<script>"
+    "document.getElementById(\"messageForm\").addEventListener(\"submit\", function(e) {"
+    "  e.preventDefault();"
+    "  var message = document.getElementById(\"messageInput\").value;"
+    "  fetch(\"/send\", {"
+    "    method: \"POST\","
+    "    body: new URLSearchParams({message: message}),"
+    "    headers: {"
+    "      \"Content-Type\": \"application/x-www-form-urlencoded\""
+    "    }"
+    "  });"
+    "  document.getElementById(\"messageInput\").value = \"\";"
+    "});"
+    ""
+    "function updateSerialData() {"
+    "  fetch(\"/read\")"
+    "    .then(response => response.text())"
+    "    .then(data => {"
+    "      document.getElementById(\"serialLog\").innerHTML = data;"
+    "      const serialLog = document.getElementById(\"serialLog\");"
+    "      serialLog.scrollTop = serialLog.scrollHeight;"    // Added this line
+    "    });"
+    "}"
+    ""
+    "function clearSerialLog() {"
+    "  fetch(\"/clear\", {"
+    "    method: \"POST\""
+    "  }).then(() => {"
+    "    document.getElementById(\"serialLog\").innerHTML = \"\";"
+    "  });"
+    "}"
+    ""
+    "setInterval(updateSerialData, 1000);"
+    "</script>"
+    "</body>"
+    "</html>"
+  );
 }
 
 void handleSend() {
   if (server.hasArg("message")) {
     String message = server.arg("message");
     mySerial.println(message);
-    serialLog += "Sent: " + message + "<br>";
+    serialLog += "Sent: " + message + "\n";    // Using single backslash for newline
     server.send(200, "text/plain", "Message sent");
   } else {
     server.send(400, "text/plain", "No message received");
@@ -140,10 +150,13 @@ void handleRead() {
   while (mySerial.available()) {
     receivedMessage += mySerial.readStringUntil('\n');
   }
-  
   if (receivedMessage.length() > 0) {
-    serialLog += "Received: " + receivedMessage + "<br>";
+    serialLog += "Received: " + receivedMessage + "\n";    // Using single backslash for newline
   }
-  
   server.send(200, "text/html", serialLog);
+}
+
+void handleClear() {
+  serialLog = "";
+  server.send(200, "text/plain", "Log cleared");
 }
